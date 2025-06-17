7e45f37c9396c82770a24da4f327b04be0fb15e9
Ganeshkishore29 <ganeshkishores29@gmail.com>
2025-06-17 14:02:24 +0530

    clear few warnings

--- smart_waste_frontend/src/pages/Feedback.js
+++ smart_waste_frontend/src/pages/Feedback.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import  { useEffect, useState } from 'react';
+
 
 export const Feedback = () => {
   const [rating, setRating] = useState(5);
